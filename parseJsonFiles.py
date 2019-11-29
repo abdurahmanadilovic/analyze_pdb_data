15ac6302f4a1d48e62d11095965af07d98ffcf6a
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2019-11-29 21:20:38 +0100

    persist local_scores dict to .csv file for each .json file

--- parseJsonFiles.py
+++ parseJsonFiles.py
@@ -9,15 +9,9 @@ def build_csv_dict(data, final_dict):
             if key == 'local_scores':
                 inner_dict = data[key]
                 inner_dict['local_scores'] = inner_dict[list(inner_dict.keys())[0]]
-                del inner_dict[list(inner_dict.keys())[0]]
-                build_csv_dict(inner_dict, final_dict)
+                final_dict['local_scores'] = inner_dict['local_scores']
             else:
                 build_csv_dict(data[key], final_dict)
-        else:
-            if isinstance(data[key], str):
-                final_dict[key] = data[key].rstrip()
-            else:
-                final_dict[key] = data[key]
 
     return final_dict
 
@@ -39,11 +33,13 @@ def main():
             csv_file_path = os.path.join(dirname, filename.split(".")[0] + ".csv")
             json_files.append((csv_file_path, build_csv_dict(data, {})))
 
-    with open(os.path.join('.', 'files', 'results.csv'), 'w') as file:
-        results_file = csv.writer(file)
-        results_file.writerow(json_files[0][1].keys())
-        for json_file in json_files:
-            results_file.writerow(json_file[1].values())
+    for json_file in json_files:
+        with open(json_file[0], 'w') as file:
+            results_file = csv.writer(file)
+            results_file.writerow(json_files[0][1].keys())
+            for values in json_file[1].values():
+                for value in values:
+                    results_file.writerow([value])
 
 
 main()
